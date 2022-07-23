0da77a946ca9d4469a6e8ba312932b5baeb0fb93
Elena <gudulina@mail.ru>
2022-07-24 01:03:25 +0700

    Начало решения Задачи 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,24 +5,36 @@ M = 1; N = 5 -> "2, 4"
 M = 4; N = 8 -> "4, 6, 8"
 */
 
+int InputNumbers(string text)
+{
+    Console.Write(text);
+    int temp = int.Parse(Console.ReadLine());
+    return temp;
+}
+
+
+int EvenNumbers(int m, int n)
+{
+  int temporary = m;
+  if (temporary > n)
+    {
+      m = n;
+      n = temporary;
+    }
+  int count = m;
+  while (count <= n)    
+    if (count % 2 == 0)
+      {
+        Console.Write($"{count}, ");
+        count++);
+      }
+  return;
+}
+
+
 int m = InputNumbers("Введите число M: ");
 int n = InputNumbers("Введите число N: ");
 
-int temporary = m;
-if (temporary > n) 
-{
-    m = n;
-    n = temporary;
-}    
+EvenNumbers(m, n);
 
-//PrintSumm(m, n);
-
-void PrintSumm(int m, int n)
-{  
-  if (n == m)
-  {
-    Console.Write($"Сумма элементов= {summ} ");
-    return;
-  }
-  PrintSumm(m, n - 1, summ);
-}
+Console.WriteLine($"{m}, {n}");
